542d7f4e7560e81ffb1be28fc3a2f60eccaa9585
Roman <roman.gajardo@gmail.com>
2020-12-02 20:29:08 -0300

    buscar completo

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ session_start();
 <body>
     <div class="container">
         <div class="row">
-            <div class="col l4 m12 s12"></div>
-            <div class="col l4 m12 s12 center">
+            <div class="col l2 m12 s12"></div>
+            <div class="col l8 m12 s12 center">
                 <h4>Buscar Receta por rut</h4>
                 <form action="controllers/BuscarRecetaRut.php" method="POST">
                     <input type="text" placeholder="Rut Cliente" name="rut">
@@ -26,8 +26,34 @@ session_start();
                 <p>
 
                     <?php if (isset($_SESSION['receta_rut'])) { ?>
-                        <?php $arr = $_SESSION['receta_rut'];  ?>
-                        <?php print_r($arr); ?>
+                        <?php $receta_list = $_SESSION['receta_rut'];  ?>
+                        <?php // print_r($receta_list); 
+                        ?>
+
+                        <table>
+                            <tr>
+                                <th>Cliente</th>
+                                <th>Armazón</th>
+                                <th>Tipo Cristal</th>
+                                <th>Fecha Retiro</th>
+                                <th></th>
+                            </tr>
+                            <?php foreach ($receta_list as $receta) { ?>
+                                <td><?= $receta['nombre_cliente'] ?></td>
+                                <td><?= $receta['armazon'] ?></td>
+                                <td><?= $receta['tipo_cristal'] ?></td>
+                                <td><?= $receta['fecha_retiro'] ?></td>
+                                <td>
+                                    <button name="id_receta" value="<?= $receta['id'] ?>" class="btn-small blue">
+                                        detalle
+                                    </button>
+                                </td>
+
+                            <?php } ?>
+
+
+                        </table>
+
 
                     <?php } else {
                         print "NONE";
